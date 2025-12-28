2724aeffee36e7a2876f800ac42763f7c9d3c5e3
KwaHash <cosmosray001@gmail.com>
2025-12-28 23:08:52 +0900

    feat: header and footer

---
+++ footer.php
@@ -0,0 +1,26 @@
+<div id="a-footer" data-float="false" class="a-footer   cssskin-_area_footer">
+	<footer>
+		<div class="site_frame">
+			<div id="bk51256" class="b-plain c-space_normal c-sp-space_normal cssskin-_block_footer" data-bk-id="bk51256"
+				data-shared="true" data-sharedname="_コピーライト">
+				<div class=" column -column1">
+					<div class="c-credit c-center">
+						<span class=" d-ming c-center">Copyright Takewaka Tokyo Legal Office. All Rights Reserved.</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+</div>
+</div>
+<script src="<?php echo esc_url(home_url('/_module/lib/lib.js?v=17.0.5')); ?>" defer></script>
+<script src="<?php echo esc_url(home_url('/_module/scripts/bind.js?v=17.0.5')); ?>" id="script-js" defer></script>
+<!--[if lt IE 9]><script src="_module/lib/html5shiv.min.js?v=17.0.5"></script><![endif]-->
+<!-- custom_tags_start -->
+<script defer type="text/javascript">
+</script>
+<!-- custom_tags_end -->
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="ja-JP">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <link rel="dns-prefetch" href="//module.bindsite.jp">
+  <link rel="dns-prefetch" href="//webfont-pub.weblife.me">
+  <link rel="dns-prefetch" href="https://smoothcontact.jp">
+  <meta name="robots" content="index">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="会社設立・商業登記・相続・不動産登記・債務整理に強い渋谷区の司法書士「武若司法書士東京事務所」 複雑化した法的手続きをトータルサポート。緊急対応、日本全国対応可｜渋谷区の武若司法書士">
+  <meta property="og:title" content="商業登記・相続・債務整理に強い司法書士｜東京・渋谷区「武若司法書士東京事務所」">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="商業登記・相続・債務整理に強い司法書士｜東京・渋谷区「武若司法書士東京事務所」">
+  <meta property="og:description"
+    content="会社設立・商業登記・相続・不動産登記・債務整理に強い渋谷区の司法書士「武若司法書士東京事務所」 複雑化した法的手続きをトータルサポート。緊急対応、日本全国対応可｜渋谷区の武若司法書士">
+  <meta name="twitter:card" content="">
+  <meta name="twitter:image" content="">
+  <meta name="note:card" content="">
+  <link rel="canonical" href="http://">
+  <link rel="apple-touch-icon-precomposed"
+    href="<?php echo esc_url(home_url('/_src/30928/favicon_20240207233952050.png')); ?>">
+  <title>商業登記や相続・債務整理に強い司法書士｜東京・渋谷区「武若司法書士東京事務所」</title>
+  <link rel="shortcut icon" href="<?php echo esc_url(home_url('/_src/30927/favicon_(1)_20240207233758268.ico')); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo esc_url(home_url('/_module/styles/bind.css?v=17.0.5')); ?>"
+    id="bind-css">
+  <link rel="stylesheet" type="text/css" href="<?php echo esc_url(get_stylesheet_directory_uri() . '/style.css'); ?>">
+  <link rel="stylesheet" type="text/css"
+    href="<?php echo esc_url(home_url('/_dress/b15009/base.css?1760419919239')); ?>">
+  <link rel="stylesheet" type="text/css"
+    href="<?php echo esc_url(home_url('/_dress/b08c0a53965f4be1858e6959a0023bc2/base.css?1760419919239')); ?>">
+  <link rel="stylesheet" type="text/css"
+    href="<?php echo esc_url(home_url('/_dress/b15009WidgetBanner/base.css?1760419919239')); ?>">
+  <link rel="stylesheet" type="text/css"
+    href="<?php echo esc_url(home_url('/_dress/b15009Bilboard/base.css?1760419919239')); ?>">
+  <link rel="stylesheet" type="text/css"
+    href="<?php echo esc_url(home_url('/_dress/b15009Contents/base.css?1760419919239')); ?>">
+  <link rel="stylesheet" type="text/css"
+    href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:200,300,regular,500,600,700,800,900&amp;display=swap">
+  <link rel="stylesheet" type="text/css"
+    href="https://fonts.googleapis.com/css?family=Zen+Maru+Gothic:300,regular,500,700,900&amp;display=swap">
+  <link rel="stylesheet" type="text/css" href="<?php echo esc_url(home_url('/index.css?1760419919244c0100')); ?>"
+    id="page-css">
+  <link rel="stylesheet" type="text/css" href="https://fonts.fontplus.dev/v1/css/jOW3USBm" id="fontplus-css1">
+  <link rel="stylesheet" type="text/css" href="https://fonts.fontplus.dev/v1/css/1YLRAG6A" id="fontplus-css2">
+  <link rel="stylesheet" type="text/css" href="https://fonts.fontplus.dev/v1/css/t8EwrwXo" id="fontplus-css3">
+  <link rel="stylesheet" type="text/css"
+    href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,200,300,regular,500,600,700,800,900&amp;display=swap">
+  <link rel="stylesheet" type="text/css" href="<?php echo esc_url(home_url('/_webfonts.css?1760419919249')); ?>"
+    id="webfont-css">
+  <link rel="stylesheet" type="text/css"
+    href="<?php echo esc_url(home_url('/_csslinkbtn/btn-import.css?1707317263877')); ?>" id="linkbtn-css">
+    <link rel="stylesheet" type="text/css"
+    href="style.css" id="linkbtn-css">
+  <!-- analytics_tags_position -->
+</head>
+
+<body id="l-1" class="l-1 -dress_b15009 bd-webp" data-type="responsive" data-fontplus="1" data-page-animation="fade">
+  <div id="page" class="bg-window  animsition">
+    <div class="bg-document">
+      <noscript>
+        <div id="js-off">
+          <img src="<?php echo esc_url(home_url('/_module/images/noscript.gif')); ?>"
+            alt="Enable JavaScript in your browser. このウェブサイトはJavaScriptをオンにしてご覧下さい。">
+        </div>
+      </noscript>
+      <div id="a-header" data-float="false" class="a-header   cssskin-_area_header">
+        <header>
+          <div class="site_frame">
+            <section>
+              <div id="bk51395"
+                class="b-both_diff -left_large -sp-single_column c-sp-space_init bd-webp -dress_b08c0a53965f4be1858e6959a0023bc2 cssskin-_block_header init-block_animation"
+                data-bk-id="bk51395" data-shared="true" data-sharedname="head" data-animated="fadeIn">
+                <div class=" g-column">
+                  <div class=" column -col8 -col_main">
+                    <div class="c-lead">
+                      <div class="c-img   ">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" data-mypid="3185">
+                          <picture>
+                            <source type="image/webp"
+                              srcset="<?php echo esc_url(home_url('/_src/30921/img20240207223343030277.webp?v=1707317263877')); ?>">
+                            <img
+                              src="<?php echo esc_url(home_url('/_src/30921/img20240207223343030277.png?v=1707317263877')); ?>"
+                              width="6000" height="1540" alt="" id="imgsrc30921_1" class=" c-no_adjust" loading="lazy">
+                          </picture>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class=" column -col4 -col_sub">
+                    <nav>
+                      <div id="js-globalNavigation" data-slide-type="upSlide" data-btn-position="rightTop_fixed">
+                        <div class=" -menu_right">
+                          <ul class="c-menu -menu_a menu-parts">
+                            <li class=" c-left"><a href="<?php echo esc_url(home_url('/')); ?>" data-mypid="3185" class=" c-current">TOP</a></li>
+                            <li class=" c-left"><a href="<?php echo esc_url(home_url('/#aisatsu')); ?>" data-mypid="3185" class=" js-link_scroller">代表挨拶</a>
+                            </li>
+                            <li class=" c-left"><a href="<?php echo esc_url(home_url('/#aboutus')); ?>" data-mypid="3185" class=" js-link_scroller">事務所概要</a>
+                            </li>
+                            <li class=" c-left"><a href="<?php echo esc_url(home_url('/#contact')); ?>" data-mypid="3185" class=" js-link_scroller"><span
+                                  class="bindicon-calendar"></span>お問い合わせ</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div id="spNavigationTrigger" class=" c-sp_navigation_btn"></div>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </header>
+      </div>
+      <div id="a-ghost_header" data-effect="fade" data-float="true"
+        class="a-ghost_header js-ghost_mode  cssskin-_block_ghost_header">
+        <header>
+          <div class="site_frame">
+            <section>
+              <div id="bk51396"
+                class="b-plain c-space_normal c-sp-space_init c-sp-padding_narrow bd-webp -dress_b08c0a53965f4be1858e6959a0023bc2 cssskin-_block_ghost_header init-block_animation"
+                data-bk-id="bk51396" data-shared="true" data-sharedname="head2" data-animated="fadeIn">
+                <div class=" g-column -col3 -sp-col1">
+                  <div class=" column -column1">
+                    <div class="c-lead">
+                      <div class="c-img   ">
+                        <a href="./" data-mypid="3185">
+                          <picture>
+                            <source type="image/webp"
+                              srcset="<?php echo esc_url(home_url('/_src/30924/img20240207223343030277.webp?v=1707317263877')); ?>">
+                            <img
+                              src="<?php echo esc_url(home_url('/_src/30924/img20240207223343030277.png?v=1707317263877')); ?>"
+                              width="6000" height="1540" alt="" id="imgsrc30924_1" class=" c-no_adjust" loading="lazy">
+                          </picture>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class=" column -column2">
+                    <nav>
+                      <div class=" -menu_right">
+                        <ul class="c-menu -menu_a menu-parts">
+                          <li><a href="<?php echo esc_url(home_url('/')); ?>" data-mypid="3185" class=" c-current">TOP</a></li>
+                          <li><a href="<?php echo esc_url(home_url('/#aisatsu')); ?>" data-mypid="3185" class=" js-link_scroller">代表挨拶</a></li>
+                          <li><a href="<?php echo esc_url(home_url('/#aboutus')); ?>" data-mypid="3185" class=" js-link_scroller">事務所概要</a></li>
+                          <li><a href="<?php echo esc_url(home_url('/#contact')); ?>" data-mypid="3185" class=" js-link_scroller"><span
+                                class="bindicon-calendar"></span>お問い合わせ</a></li>
+                        </ul>
+                      </div>
+                    </nav>
+                  </div>
+                  <div class=" column -column3 -column-lasts">
+                    <div class="c-none">
+                      <a href="https://maps.app.goo.gl/to6pFRd9vzHyVRwq6" target="_blank" rel="noopener noreferrer"
+                        class="header_circle1"><span class="bindicon-location"></span></a>
+                    </div>
+                    <div class="c-none">
+                      <a href="#" class="header_circle2 js-link_scroller"><span class="bindicon-instagram"></span></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </header>
+      </div>
